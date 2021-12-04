e4cff81c91fcac85ad63f7c8f13f4dfd05636a6c
tung2389 <luukhanhtung2002@gmail.com>
2021-12-03 21:55:10 -0500

    Fix prim method and add constructor based on filename for graph class - project 7

--- CS271/7-project/graph.cpp
+++ CS271/7-project/graph.cpp
@@ -1,24 +1,47 @@
+#include<iostream>
+#include<fstream>
+#include<string>
 #include "graph.h"
 #include "pq.h"
 using namespace std;
 
+Graph::Graph(string fileName) {
+    ifstream graph(fileName);
+    if(graph.is_open()) {
+        int numVertexes;
+        graph >> numVertexes;
+        for(int i = 0; i < numVertexes; i++) {
+            adjList.push_back(new Vertex(i));
+        }
+        for(int i = 0; i < numVertexes; i++) {
+            for(int j = 0; j < numVertexes; j++) {
+                int weightij;
+                graph >> weightij;
+                if(i != j && weightij > 0) {
+                    adjList[i]->edges.push_back(new Edge(adjList[j], weightij));
+                }
+            }
+        }
+    }
+}
+
 /*
 In this implementation, I use a trick to avoid using the decreaseKey operation of the priority queue, since
 the decreaseKey operations requires us to save the information about the index of different vertexes in the priority queue, which keeps changing overtime.
-In this implementation, instead of using the decrease key operation when update shortest distance to vertex, I just insert that vertex again in the 
+In this implementation, instead of using the decrease key operation when update minimum weight of a vertex, I just insert that vertex again in the 
 pq, and the pq will automatically push that vertex up. To deal with duplicated vertex in the queue, I check if a vertex has already been put into
 the MST in O(1). The memory efficiency will not be impacted much, since we have the maximum of O(E) in the priority queue, 
 while we already using O(V + E) memory to store graph. 
 */
 void Graph::prim(int root) {
     for(Vertex *vertex : adjList) {
-        vertex->dist = INT_MAX;
+        vertex->minWeight = INT_MAX;
         Vertex->parent = nullptr;
     }
 
-    adjList[root]->dist = 0;
+    adjList[root]->minWeight = 0;
     MinPriorityQueue<Vertex> pq(adjList);
-    
+
     // If the vertex i is already in the MST, then inMST[i] = true, false otherwise.
     vector<bool> inMST(adjList.size(), false);
 
@@ -30,8 +53,8 @@ void Graph::prim(int root) {
         inMST[top->key] = true;
 
         for(Edge *edge : top->edges) {
-            if(edge->dest->dist > top->dist + edge->weight) {
-                edge->dest->dist = top->dist + edge->weight;
+            if(edge->weight < edge->dest->minWeight) {
+                edge->dest->minWeight = edge->weight ;
                 edge->dest->parent = top;
                 // Instead of calling decreaseKey and deal with indexes, just insert the vertex again and let the pq put it at the right place
                 pq.insert(edge->dest);
--- CS271/7-project/vertex.h
+++ CS271/7-project/vertex.h
@@ -7,23 +7,27 @@ using namespace std;
 class Vertex {
     public:
         int key;
-        int dist; // distance from source vertex.
+        int minWeight; // minimum weight of any edge connecting v to a vertex in the tree
         Vertex *parent;
         vector<Edge *> edges;
 
         bool operator < (const Vertex& other) {
-            return (this->dist < other.dist);
+            return (this->minWeight < other.minWeight);
         } 
 
         bool operator > (const Vertex& other) {
-            return (this->dist > other.dist);
+            return (this->minWeight > other.minWeight);
         } 
+
+        Vertex(int key) : key(key) {};
 };
 
 class Edge {
     public:
         Vertex *dest;
         int weight;
+
+        Edge(Vertex *dest, int weight) : dest(dest), weight(weight) {};
 };
 
 #endif VERTEX_H
